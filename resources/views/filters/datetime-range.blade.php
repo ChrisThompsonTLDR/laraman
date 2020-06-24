99fcb33c64886590aa593ededc596c4059a4d2b8
James Wagoner <jameswagoner@gmail.com>
2020-06-23 17:28:21 -0700

    Laravel 6.x does not support Str::of()

--- resources/views/filters/datetime-range.blade.php
+++ resources/views/filters/datetime-range.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group filter">
-    <div id="filter{{ Str::of($filter->field)->title() }}">
+    <div id="filter{{ Str::title($filter->field) }}">
         @if(!isset($filter->label) || $filter->label === true)<div><strong>{!! $filter->display !!}</strong></div>@endif
         {!! Form::text('filter_' . $filter->field . '_start', isset($params['filter_' . $filter->field . '_start']) ? $params['filter_' . $filter->field . '_start'] : null, ['class' => 'form-control']) !!}
         to
